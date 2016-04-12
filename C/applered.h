e17f468162eec51798adc47b3048349ebc2a63d2
Nikki N <lnikolenko3@gatech.edu>
2016-04-12 16:39:23 -0400

    added new project

---
+++ C/applered.h
@@ -0,0 +1,3 @@
+# define APPLERED_WIDTH 8
+# define APPLERED_HEIGHT 8
+const unsigned short applered_data[64];
---
+++ C/snake_title.h
@@ -0,0 +1,3 @@
+# define SNAKE_TITLE_WIDTH 240
+# define SNAKE_TITLE_HEIGHT 160
+const unsigned short snake_title_data[38400];
